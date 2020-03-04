7bf50339d550e81ba283d334d2c5ca7e66506ef3
Mike Slomczynski <mike.slomczynski@gmail.com>
2020-03-04 11:27:28 -0600

    Release Version - 0.0.1

---
+++ init.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import cv2 as cv
+import tensorflow as tf
+import sklearn
+import selenium
+import numpy as np
+print( cv.__version__ )
+print(tf.__version__)
+print(sklearn.__version__)
+print(selenium.__version__)
+print(np.__version__)
+#image captioning
+#object detection
