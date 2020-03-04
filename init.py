3558ece18d07a89c110d47997d973b0eca187e32
Mike Slomczynski <mike.slomczynski@gmail.com>
2020-03-04 12:31:47 -0600

    Release Version - 0.0.2

--- init.py
+++ init.py
@@ -1,20 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-
+### Imports
+###############################################################################
 import cv2 as cv
 import tensorflow as tf
 import sklearn
 import selenium
 import numpy as np
-print( cv.__version__ )
-print(tf.__version__)
-print(sklearn.__version__)
-print(selenium.__version__)
-print(np.__version__)
+###############################################################################
+
+
+### Classes
+###############################################################################
+class PyBot:
+    """
+    Takes in computer vision and returns computer action.
+    """
+
+    def __init__(self):
+        return
+
+
+class Input:
+    """
+    Computer vision.
+    """
+
+    def __init__(self):
+        return
+
+
+class Output:
+    """
+    Computer actions.
+    """
+
+    def __init__(self):
+        return
+###############################################################################
+
+
+### Functions
+###############################################################################
+def checkVersion():
+    print(cv.__version__)
+    print(tf.__version__)
+    print(sklearn.__version__)
+    print(selenium.__version__)
+    print(np.__version__)
+    return
+
+def main():
+    return
+###############################################################################
+
+
+### Entry Point
+###############################################################################
+if __name__ == "__main__":
+    main()
+###############################################################################
+
+
+
 #image captioning
 #object detection
